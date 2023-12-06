73b38bc10cd2409e0848a154c0e4de6b0e9850a9
Nilam <shahnilam812@gmail.com>
2023-12-06 18:52:01 +0530

    Initial Commit

---
+++ AfricaApp/AfricaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaAppApp.swift
+//  AfricaApp
+//
+//  Created by Nilam Shah on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
